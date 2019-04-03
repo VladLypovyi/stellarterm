66b5d72c4118710c6d4f643d6ce1048a0402a9d4
little-wave <little-wave@lobstr.co>
2019-04-03 15:53:30 +0300

    Sorting done, need review

--- src/components/AssetListRows.jsx
+++ src/components/AssetListRows.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import _ from 'lodash';
 import AssetCard2 from './AssetCard2';
 import Printify from '../lib/Printify';
 import directory from '../directory';
@@ -7,6 +8,18 @@ import Format from '../lib/Format';
 import Ticker from '../lib/api/Ticker';
 
 export default class AssetListRows extends React.Component {
+    static getAssetData(asset) {
+        return {
+            assetName: asset.code,
+            priceXLM: asset.price_XLM,
+            priceUSD: asset.price_USD,
+            volume24h: asset.volume24h_USD,
+            change24h: asset.change24h_USD,
+            isXLMNative: asset.id === 'XLM-native',
+            asset,
+        };
+    }
+
     static getPriceXLM(isXLMNative, asset) {
         if (isXLMNative) {
             return Printify.lightenZeros('1.0000000');
@@ -17,22 +30,25 @@ export default class AssetListRows extends React.Component {
             : '-';
     }
 
-    getAssetDataRows(isXLMNative, asset) {
+    getAssetRow(isXLMNative, asset) {
         const priceUSD = asset.price_USD ? (
             <span>${Printify.lightenZeros(asset.price_USD.toString(), Format.niceNumDecimals(asset.price_USD))}</span>
-        ) : '-';
+        ) : (
+            '-'
+        );
 
         const volume24h = asset.volume24h_USD
             ? `$${asset.volume24h_USD.toLocaleString('en-US', {
                 minimumFractionDigits: 0,
                 maximumFractionDigits: 0,
-            })}` : '$0';
+            })}`
+            : '$0';
 
         const priceXLM = AssetListRows.getPriceXLM(isXLMNative, asset);
         const change24h = this.checkFor24hChanges(isXLMNative, asset);
-        const tradeLink = asset.topTradePairSlug ?
-            (<span className="AssetList__asset__amount__trade">trade</span>) :
-            null;
+        const tradeLink = asset.topTradePairSlug ? (
+            <span className="AssetList__asset__amount__trade">trade</span>
+        ) : null;
 
         const assetListClass = 'AssetList__asset__amount';
 
@@ -54,11 +70,9 @@ export default class AssetListRows extends React.Component {
         let change24hPercentage;
         const { ticker } = this.props;
 
-        if (isXLMNative) {
-            change24hPercentage = ticker.data._meta.externalPrices.USD_XLM_change;
-        } else {
-            change24hPercentage = asset.change24h_USD;
-        }
+        isXLMNative
+            ? (change24hPercentage = ticker.data._meta.externalPrices.USD_XLM_change)
+            : (change24hPercentage = asset.change24h_USD);
 
         if (change24hPercentage === null || change24hPercentage === undefined) {
             return '-';
@@ -68,18 +82,38 @@ export default class AssetListRows extends React.Component {
         return <span className="AssetList__asset__changePositive">{change24hPercentage.toFixed(2)}%</span>;
     }
 
+    sortAssets(allAssets, xlmNative) {
+        const { sortBy, sortType } = this.props;
+        const direction = new Map([[true, 'asc'], [false, 'desc']]);
+        const isAscSort = sortType !== null ? direction.get(sortType) : '';
+        const sortedAssets = _.orderBy(allAssets, sortBy, isAscSort);
+        sortedAssets.unshift(xlmNative);
+        return sortedAssets;
+    }
+
     render() {
         const { ticker, limit } = this.props;
+        const allAssets = [];
+        let xlmNative;
 
-        return ticker.data.assets.map((asset, index) => {
+        ticker.data.assets.map((asset, index) => {
             const directoryAsset = directory.getAssetByAccountId(asset.code, asset.issuer);
             const limitIsReached = limit && index >= limit;
             const assetIsUndefined = directoryAsset === null || directoryAsset.unlisted;
 
-            if (limitIsReached || assetIsUndefined) { return null; }
+            if (limitIsReached || assetIsUndefined) {
+                return null;
+            }
 
-            const isXLMNative = asset.id === 'XLM-native';
-            const assetRow = this.getAssetDataRows(isXLMNative, asset);
+            asset.id !== 'XLM-native'
+                ? allAssets.push(this.constructor.getAssetData(asset))
+                : (xlmNative = this.constructor.getAssetData(asset));
+            return null;
+        });
+
+        return this.sortAssets(allAssets, xlmNative).map((assetInfo) => {
+            const { asset, isXLMNative } = assetInfo;
+            const assetRow = this.getAssetRow(isXLMNative, asset);
 
             return (
                 <a href={`#exchange/${asset.topTradePairSlug}`} key={`asset-${asset.id}`} className="AssetList__asset">
@@ -96,4 +130,6 @@ export default class AssetListRows extends React.Component {
 AssetListRows.propTypes = {
     ticker: PropTypes.instanceOf(Ticker).isRequired,
     limit: PropTypes.number,
+    sortBy: PropTypes.string,
+    sortType: PropTypes.bool,
 };
